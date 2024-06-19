2cedf4a90f770ffb8359c11ebbc113f8921b8cd4
emablanco1800 <emablanco1800@gmail.com>
2024-06-18 23:54:30 -0300

    eliminacion de link mongoDB

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
   try {
-      mongoose.connect("mongodb+srv://admin:upuruwKx92dtALCg@coderback3.xredhse.mongodb.net/productosdb")
+      mongoose.connect("")
       console.log("MongoDB connected");
   } catch (error) {
     console.log(`Error: ${error}`);
